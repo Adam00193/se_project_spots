8a0034f84e34d08aaddc4f30fd895d810158ea55
Adam <pettadam347@gmail.com>
2025-09-06 16:41:38 -0600

    Added js feature to replace previous title and desripion in profile section and to console log input for new post modal

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileCloseBtn = editProfileModal.querySelector(
   ".modal__close-button"
 );
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 
 const editProfileNameInput = editProfileModal.querySelector(
   "#profile-name-input"
@@ -15,10 +16,15 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostBtn = document.querySelector(".profile__new-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 
+const newPostForm = newPostModal.querySelector(".modal__form");
+
+const newPostLinkInput = newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#card-caption-input");
+
 const editProfileNameEl = document.querySelector(".profile__title");
 const editProfileDescriptionEl = document.querySelector(
   ".profile__description"
-)
+);
 
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = editProfileNameEl.textContent;
@@ -38,3 +44,24 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  editProfileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  editProfileNameEl.textContent = editProfileNameInput.value;
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handeNewPostSubmit(evt) {
+  evt.preventDefault();
+
+  console.log(newPostLinkInput.value + " Link");
+  console.log(newPostCaptionInput.value + " Caption");
+
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+newPostForm.addEventListener("submit", handeNewPostSubmit);
