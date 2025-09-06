366c3030e98ed5283a6d348b3537e0fc4fb55110
Adam <pettadam347@gmail.com>
2025-09-06 16:09:06 -0600

    created js feature modifying placeholders in modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,29 @@
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileBtn = document.querySelector(".profile__edit-btn");
-const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-button");
+const editProfileCloseBtn = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostBtn = document.querySelector(".profile__new-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
 
+const editProfileNameEl = document.querySelector(".profile__title");
+const editProfileDescriptionEl = document.querySelector(
+  ".profile__description"
+)
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = editProfileNameEl.textContent;
+  editProfileDescriptionInput.value = editProfileDescriptionEl.textContent;
+
   editProfileModal.classList.add("modal_is-opened");
 });
 
